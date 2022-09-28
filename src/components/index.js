d08680ae1805a9a9694efb49fde9a8e19bcef724
ablahum <mriztama@gmail.com>
2022-09-29 02:28:52 +0700

    init commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import TopButtons from './TopButtons'
+import Inputs from './Inputs'
+import TimeAndLocation from './TimeAndLocation'
+import TemperatureAndDetails from './TemperatureAndDetails'
+import Forecast from './Forecast'
+
+export { TopButtons, Inputs, TimeAndLocation, TemperatureAndDetails, Forecast }
